a329fac869022271ddddea6ce49ba844e586b888
Viktor Sveda <vinix.ru@gmail.com>
2024-10-21 18:41:15 +0300

    add router in main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-from user import routes
+from user import routes as user_routes
+from posts import routes as posts_routes
 
 
 app = FastAPI()
 
-app.include_router(routes.router, prefix="/api/user", tags=["User API"])
+app.include_router(user_routes.router, prefix="/api/user", tags=["User API"])
+app.include_router(posts_routes.router, prefix="/api/posts", tags=["Posts API"])
