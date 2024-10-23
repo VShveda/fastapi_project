284881175749cbc6c8cbf37813a3a6405dc030de
Viktor Sveda <vinix.ru@gmail.com>
2024-10-23 14:45:17 +0300

    refactor alembic db

--- db/init_db.py
+++
@@ -1,8 +0,0 @@
-from db.database import Base, engine
-from posts.models import Post
-from user.models import User
-
-
-print("Creating database tables...")
-Base.metadata.create_all(bind=engine)
-print("Database tables created successfully!")
