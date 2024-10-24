536c0fabd0a90466c377d5d2b7c646e636401de6
Viktor Sveda <vinix.ru@gmail.com>
2024-10-24 12:18:26 +0300

    fix user model

--- user/models.py
+++ user/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import timedelta
+
+from sqlalchemy import Column, Integer, String, Boolean, Interval
 from sqlalchemy.orm import relationship
 
 from db.database import Base
@@ -10,6 +12,8 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    auto_reply_enabled = Column(Boolean, default=False)
+    reply_time = Column(Interval, default=timedelta(minutes=1))
 
     posts = relationship("Post", back_populates="user")
     comments = relationship("Comment", back_populates="user")
